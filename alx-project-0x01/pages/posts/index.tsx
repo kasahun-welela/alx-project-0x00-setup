6c00330db945c56061f8de2fb11e51a7f30565bd
kasahun-welela <kasahunwelela1@gmail.com>
2025-05-28 22:23:24 +0300

    create folder structure for the project

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,9 @@
+const Post: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-7xl font-thin">Post Page</h1>
+    </div>
+  );
+};
+
+export default Post;
